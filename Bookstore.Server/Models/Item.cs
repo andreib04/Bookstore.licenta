669d2de176f0d1f4005e89f33f5c32e34720c620
Andrei R <andrei_raul04@yahoo.com>
2024-11-05 20:46:21 +0200

    Add project files.

---
+++ Bookstore.Server/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookstore.Server.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+	 	public float Price { get; set; }
+		public int Stock {  get; set; }
+		public Category CategoryId { get; set; } = null!;
+		public string ItemType { get; set; } = string.Empty;
+	}
+}
