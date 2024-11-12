ac48604409f0e5224a8292f524b98ea59ce360ed
Andrei Bucșa <andrei_raul04@yahoo.com>
2024-11-12 10:26:04 +0200

    update

--- Bookstore.Server/Models/Item.cs
+++ Bookstore.Server/Models/Item.cs
@@ -3,11 +3,11 @@
 	public class Item
 	{
 		public int Id { get; set; }
-		public string Title { get; set; } = string.Empty;
-		public string Description { get; set; } = string.Empty;
+		public string? Title { get; set; }
+		public string? Description { get; set; }
 	 	public float Price { get; set; }
 		public int Stock {  get; set; }
-		public Category CategoryId { get; set; } = null!;
-		public string ItemType { get; set; } = string.Empty;
+		public Category? CategoryId { get; set; }
+		public string? ItemType { get; set; }
 	}
 }
